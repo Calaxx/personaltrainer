0ae1d32f359db5aa04251603160cd13553dbf886
Calaxx <emmi.joensuu@gmail.com>
2021-12-13 22:53:15 +0200

    added delete add edit for customerlist

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -3,8 +3,8 @@ import React, { useEffect, useState} from "react";
 import { AgGridReact } from 'ag-grid-react';
 import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
-//import AddCar from './AddCar';
-//import EditCar from './EditCar';
+import AddCustomer from './AddCustomer';
+import EditCustomer from './EditCustomer';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
@@ -25,6 +25,64 @@ function Customerlist() {
         .catch(err => console.error(err))
     }
 
+    //delete customer
+    const deleteCustomer = (url) => {
+        console.log(url);
+        if (window.confirm('Are you sure?')) {
+            fetch(url, { method: 'DELETE' })
+            .then(response => {
+                if (response.ok) {
+                    fetchCustomers();
+                }
+                else {
+                    alert('Something went wrong');
+                }
+            })
+            .catch(err => console.error(err))
+        }
+    }
+    //delete customer
+
+    //add customer
+    const addCustomer = (customer) => {
+        fetch('https://customerrest.herokuapp.com/api/customers', {
+            method: 'POST',
+            headers: { 'Content-type' : 'application/json' },
+            body: JSON.stringify(customer)
+        })
+        .then(response => {
+            if(response.ok) {
+                fetchCustomers();
+            }
+            else {
+                alert('New customer could not be created')
+            }
+        })
+        .catch(err => console.error(err))
+    }
+    //add customer
+
+    //update customer
+    const updateCustomer = (url, updatedCustomer) => {
+        fetch(url, {
+            method: 'PUT',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(updatedCustomer)
+        })
+        .then(response =>{
+            if(response.ok) {
+                fetchCustomers();
+                setMsg('Customer edited successfully');
+                setOpen(true);
+            }
+            else {
+                alert('Customer info could not be edited');
+            }
+        })
+        .catch(err => console.error(err))
+    }
+    //update customer
+
     const columns = [
         {headerName: 'First name', field: 'firstname', sortable: true, filter: true},
         {headerName: 'Last name', field: 'lastname', sortable: true, filter: true},
@@ -33,17 +91,43 @@ function Customerlist() {
         {field: 'city', sortable: true, filter: true},
         {field: 'email', sortable: true, filter: true},
         {field: 'phone', sortable: true, filter: true},
+        {
+            headerName: '',
+            field: 'links.0.href',
+            width: 150,
+            cellRendererFramework: params => <EditCustomer updateCustomer={updateCustomer} params={params} />
+        },
+        {
+            headerName: ' ',
+            field: 'links.0.href',
+            width: 120,
+            cellRendererFramework: params =>
+            <Button size="small"
+            onClick={() => deleteCustomer(params.value)}
+            color="error">
+                Delete
+            </Button>
+        }
     ]
 
     return(
-        <div className="ag-theme-material" style={{height: 400, width: '80%', margin: 'auto', textAlign: 'left'}}>
-            <AgGridReact
-                rowData={customers}
-                columnDefs={columns}
-                pagination={true}
-                paginationPageSize={7}
-            />            
-        </div>
+        <React.Fragment>
+            <AddCustomer addCustomer={addCustomer} />
+            <div className="ag-theme-material" style={{height: 400, width: '80%', margin: 'auto', textAlign: 'left'}}>
+                <AgGridReact
+                    rowData={customers}
+                    columnDefs={columns}
+                    pagination={true}
+                    paginationPageSize={7}
+                />            
+            </div>
+            <Snackbar
+                open={open}
+                autoHideDuration={3000}
+                onClose={() => setOpen(false)}
+                message={msg}
+            />
+        </React.Fragment>
     );
 }
 
